9e9c0909c6fe1a259328e5062d7eb71fc62e83e8
Adam Cardenas <cevaris@gmail.com>
2018-06-25 20:16:42 -0600

    added logback/slf4j

--- src/main/java/com/cevaris/ag4j/Main.java
+++ src/main/java/com/cevaris/ag4j/Main.java
@@ -1,15 +1,22 @@
 package com.cevaris.ag4j;
 
 import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 
 public class Main {
+  public static final Logger logger = LoggerFactory.getLogger(Main.class);
+
   private final static String WAT = "ERR: What do you want to search for?";
 
   public static void main(String[] args) {
-    OptionParser parser = new OptionParser("G");
+    OptionParser parser = new OptionParser("GD");
     OptionSet opts = parser.parse(args);
 
     if (args.length == 0) {
@@ -17,15 +24,29 @@ public class Main {
       System.exit(1);
     }
 
-    if (args.length > 1 && !(new File(args[1]).exists())) {
+    if (opts.has("D")) {
+      System.out.println(opts.nonOptionArguments());
+      System.out.println(String.join(",", args));
+    }
+
+    List<File> files = collectFiles(args);
+    if (files.isEmpty()) {
       System.out.println(opts.nonOptionArguments().get(0) + " " + System.getProperty("user.dir"));
-      System.exit(0);
-    } else {
-      System.out.println(opts.nonOptionArguments().get(0) + " " + new File(args[1]).getAbsolutePath());
       System.exit(0);
     }
 
-    System.out.println(opts.nonOptionArguments());
-    System.out.println(String.join(",", args));
+    System.out.println(opts.nonOptionArguments().get(0) + " " + String.join(", ", files.toString()));
+    System.exit(0);
+  }
+
+  private static List<File> collectFiles(String[] args) {
+    List<File> files = new LinkedList<>();
+    for (String arg : args) {
+      File file = new File(arg);
+      if (file.exists()) {
+        files.add(file);
+      }
+    }
+    return files;
   }
 }
